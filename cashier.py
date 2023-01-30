aaa76976b18488971c18cd6e47d6f9f578d75948
haritsf10 <fharits@gmail.com>
2023-01-30 20:27:09 +0700

    update syntax and comments

--- cashier.py
+++ cashier.py
@@ -30,59 +30,64 @@ def menu():
     print('4 Reset Transaction')
     print('5 Check Order')
     print('6 Total Transaction')
+    print('0 Exit')
     
     # Prompting user to enter any task above 
-    pilih_menu = int(input('Pilih: '))
+    pilih_menu = input('Pilih: ')
 
     # After user enter task no, respective task functions will be executed
-    if pilih_menu == 1:
-        print('Silakan tambah barang')
-        nama_item = str(input('Nama: '))
-        jumlah_item = int(input('Jumlah: '))
-        harga_item = int(input('Harga: '))
-        add.add_item(transaction, nama_item, jumlah_item, harga_item)
-        menu()
-    elif pilih_menu == 2:
-        print('1 Update Item Name')
-        print('2 Update Item Qty')
-        print('3 Update Item Price')
+    try:
+        if pilih_menu == '1':
+            print('Silakan tambah barang')
+            nama_item = str(input('Nama: '))
+            jumlah_item = int(input('Jumlah: '))
+            harga_item = int(input('Harga: '))
+            add.add_item(transaction, nama_item, jumlah_item, harga_item)
+            menu()
+        elif pilih_menu == '2':
+            print('1 Update Item Name')
+            print('2 Update Item Qty')
+            print('3 Update Item Price')
 
-        pilih_update = int(input(''))
-        
-        if pilih_update == 1:
-            nama_item_lama = str(input('Nama Item: '))
-            nama_item_baru = str(input('Nama Baru: '))
-            update.update_item_name(transaction,nama_item_lama, nama_item_baru)       
+            pilih_update = input('')
+            
+            if pilih_update == '1':
+                nama_item_lama = str(input('Nama Item: '))
+                nama_item_baru = str(input('Nama Baru: '))
+                update.update_item_name(transaction,nama_item_lama, nama_item_baru)       
+                menu()
+            elif pilih_update == '2':
+                nama_item = str(input('Nama Item: '))
+                qty_baru = int(input('Qty Baru: '))
+                update.update_item_qty(transaction, nama_item, qty_baru)
+                menu()      
+            elif pilih_update == '3':
+                nama_item = str(input('Nama Item: '))
+                price_baru = int(input('Price Baru: '))
+                update.update_item_price(transaction, nama_item, price_baru)
+                menu()
+            # If user key in no outside of available inputs, will back to menu
+            else:
+                menu()
+        elif pilih_menu == '3':
+            nama_item = str(input('Delete: '))
+            delete.delete_item(transaction, nama_item)
             menu()
-        elif pilih_update == 2:
-            nama_item = str(input('Nama Item: '))
-            qty_baru = int(input('Qty Baru: '))
-            update.update_item_qty(transaction, nama_item, qty_baru)
-            menu()      
-        elif pilih_update == 3:
-            nama_item = str(input('Nama Item: '))
-            price_baru = int(input('Price Baru: '))
-            update.update_item_price(transaction, nama_item, price_baru)
+        elif pilih_menu == '4':
+            delete.reset_transaction(transaction)
             menu()
+        elif pilih_menu == '5':
+            check.check_order(transaction)
+            menu()
+        # After total transaction, will exit program
+        elif pilih_menu == '6':
+            total.total_price(transaction)
+        # If user key in no outside of available inputs, will exit program
         else:
-            menu()
-    elif pilih_menu == 3:
-        nama_item = str(input('Delete: '))
-        delete.delete_item(transaction, nama_item)
-        menu()
-    elif pilih_menu == 4:
-        delete.reset_transaction(transaction)
-        menu()
-    elif pilih_menu == 5:
-        check.check_order(transaction)
-        menu()
-    elif pilih_menu == 6:
-        total.total_price(transaction)
-    # If user key in no outside of available inputs, menu will be displayed
-    else:
-        menu()
+            pass
+    except:
+        print('Format tidak sesuai')
 
 
-# Displaying main menu when main.py file is executed on terminal
+# Displaying menu when cashier.py file is executed on terminal
 menu()
-    
